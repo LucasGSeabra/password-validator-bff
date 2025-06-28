3877d92c8c8efd9955b80c0a91851edee30144f0
Lucas <lucasgseabra@gmail.com>
2025-06-28 12:30:33 -0300

    Chore: Cria GlobalExceptionHandler

---
+++ src/main/java/com/password_validator_bff/password_validator/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.password_validator_bff.password_validator.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError error : exception.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
