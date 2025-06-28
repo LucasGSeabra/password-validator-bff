2da70942899595ef2e9c69cd6e95a9c7cf030472
Lucas <lucasgseabra@gmail.com>
2025-06-28 12:30:13 -0300

    Chore: Adiciona chamada para PasswordValidatorService dentro do controller

--- src/main/java/com/password_validator_bff/password_validator/controller/PasswordValidatorController.java
+++ src/main/java/com/password_validator_bff/password_validator/controller/PasswordValidatorController.java
@@ -1,5 +1,9 @@
 package com.password_validator_bff.password_validator.controller;
 
+import com.password_validator_bff.password_validator.dto.PasswordRequest;
+import com.password_validator_bff.password_validator.dto.PasswordResponse;
+import com.password_validator_bff.password_validator.service.PasswordValidatorService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/validar")
 public class PasswordValidatorController {
+
+    private final PasswordValidatorService service;
+
+    public PasswordValidatorController(PasswordValidatorService service) {
+        this.service = service;
+    }
+
     @PostMapping
-    public ResponseEntity<String> validatePassword(@RequestBody String password) {
-        return ResponseEntity.ok("Requisicao autenticada recebida");
+    public ResponseEntity<PasswordResponse> validatePassword(@RequestBody @Valid PasswordRequest request) {
+        PasswordResponse response = service.validate(request.password());
+
+        return ResponseEntity.ok(response);
     }
 }
