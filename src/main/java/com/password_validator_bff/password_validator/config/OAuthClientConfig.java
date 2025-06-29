2b5a9740b32385d195c2e3b658ef7f7e098fea7e
Lucas <lucasgseabra@gmail.com>
2025-06-28 23:14:37 -0300

    Chore: Cria config do clientManager

---
+++ src/main/java/com/password_validator_bff/password_validator/config/OAuthClientConfig.java
@@ -0,0 +1,31 @@
+package com.password_validator_bff.password_validator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+
+@Configuration
+public class OAuthClientConfig {
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clients,
+            OAuth2AuthorizedClientRepository authRepo) {
+
+        OAuth2AuthorizedClientProvider provider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        DefaultOAuth2AuthorizedClientManager manager =
+                new DefaultOAuth2AuthorizedClientManager(clients, authRepo);
+        manager.setAuthorizedClientProvider(provider);
+
+        return manager;
+    }
+}
